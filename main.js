189b2d1f7623702ee11e32efbfb82b21d14b71c1
Jeff Woltjen <jeff.woltjen@gmail.com>
2020-07-31 12:05:57 -0500

    Successfully implement turn-based gameplay on DOM

--- main.js
+++ main.js
@@ -1,18 +1,19 @@
 var grid = document.querySelector('.d')
-
-// window.onload = function(){
-  var test = new Game(new Player({"Player1": "PlayerOne", "Token": "x"}), new Player({"Player2": "Player2", "Token": "o"}))
+var test = new Game(new Player({"Player1": "PlayerOne", "Token": "X"}), new Player({"Player2": "Player2", "Token": "O"}))
 
 grid.addEventListener('click', handleMainClick)
 
 function handleMainClick(event){
  if (test.player1turn === true && event.target.className === "square"){
-   return test.player1turn = false,test.player2turn = true
-   console.log("player1 has clicked a square")
- }
- if (test.player2turn === true && event.target.className === "square"){
-   test.player1turn = true;
+  event.target.innerText = ("X")
+  test.player1turn = false;
+  console.log("player1 has clicked a square")
+ test.player2turn = true;
+} else if
+  (test.player2turn === true && event.target.className === "square"){
+   event.target.innerText = ("O")
    test.player2turn = false;
    console.log("player2 has clicked a square")
+   test.player1turn = true;
  }
 }
