b54cd5a822c45b5b0ca19a0903681e6c6b4b6099
Jeff Woltjen <jeff.woltjen@gmail.com>
2020-07-31 12:39:29 -0500

    Add basic game counter function to determine if game is drawn

--- main.js
+++ main.js
@@ -6,14 +6,27 @@ grid.addEventListener('click', handleMainClick)
 function handleMainClick(event){
  if (test.player1turn === true && event.target.classList.contains("active")){
   event.target.innerText = ("X")
+  dataModelUpdater()
+  turnCounter()
   event.target.classList.remove("active")
   test.player1turn = false;
   test.player2turn = true;
 } else if
   (test.player2turn === true && event.target.classList.contains("active")){
    event.target.innerText = ("O")
+   dataModelUpdater()
+   turnCounter()
    event.target.classList.remove("active")
    test.player2turn = false;
    test.player1turn = true;
  }
+}
+function dataModelUpdater(){
+
+}
+function turnCounter(){
+    test.turnCounter++
+    if (test.turnCounter === 9){
+      console.log("The game is drawn!")
+    }
 }
